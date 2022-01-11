697fe8fb0ddf1ecc4155039105a70c50fcd57ecc
Ethan Shealey <ethan.shealey@gmail.com>
2022-01-11 09:23:17 -0500

    updating

--- index.js
+++ index.js
@@ -19,20 +19,17 @@ app.post('/api/shorturl', (req, res) => {
   const URL = req.body.url
   dns.lookup(URL.replace(/^http[s]?:\/\//, ''), (err, addr, fam) => {
     if(err) {
-      console.log(err)
       return res.json({ 'error': 'invalid url' })
     }
-    console.log('hi')
     MongoClient.connect(process.env.MONGO_URI, (err, client) => {
       if(err) return res.json(err)
-      console.log('hi2')
       client.db('url-shortner').collection('urls').findOne({ 'original_url': URL }).then((item) => {
         if(item) return res.json({ 'original_url': item.original_url, 'short_url': item.short_url })
         client.db('url-shortner').collection('urls').find().toArray((err, arr) => {
           const id = Math.max.apply(Math, arr.map((url) => { return url.short_url })) + 1
           const obj = { 'original_url': URL, 'short_url': id }
           client.db('url-shortner').collection('urls').insertOne(obj)
-          return res.json(obj) 
+          return res.json({ 'original_url': URL, 'short_url': id }) 
         })
       })
     })
