5d0e308b1b78e443632eb0346016329c6f03b7de
Ethan Shealey <ethan.shealey@gmail.com>
2022-01-29 13:23:34 -0500

    updating

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ app.post('/api/shorturl', (req, res) => {
     }
     MongoClient.connect(process.env.MONGO_URI, (err, client) => {
       if(err) return res.json(err)
-      client.db('url-shortner').collection('urls').findOne({ 'original_url': URL }).then((item) => {
-        if(item) return res.json({ 'original_url': item.original_url, 'short_url': item.short_url })
+      client.db('url-shortner').collection('urls').findOne({ original_url: URL }).then((item) => {
+        if(item) return res.json({ original_url: item.original_url, short_url: item.short_url })
         client.db('url-shortner').collection('urls').find().toArray((err, arr) => {
           let id = Math.max.apply(Math, arr.map((url) => { return url.short_url })) + 1
           if(id === -Infinity) id = 1
-          const obj = { 'original_url': URL, 'short_url': id }
+          const obj = { original_url: URL, short_url: id }
           client.db('url-shortner').collection('urls').insertOne(obj)
           console.log('INSERTED: ', obj)
-          return res.json({ 'original_url': URL, 'short_url': id }) 
+          return res.json({ original_url: URL, short_url: id }) 
         })
       })
     })
@@ -43,12 +43,12 @@ app.get('/api/shorturl/:id', (req, res) => {
   console.log('GETTING URL: ', req.params.id)
   MongoClient.connect(process.env.MONGO_URI, (err, client) => {
     client.db('url-shortner').collection('urls').findOne({ short_url: req.params.id * 1}, (err, item) => {
-     if(err) return res.json({ 'error': err })
+     if(err) return res.json({ error: err })
      if(item) {
        console.log('REDIRECTING TO: ', item.original_url)
        return res.redirect(item.original_url)
      }
-     else return res.json({ 'error': 'No short URL found for the given input' })
+     else return res.json({ error: 'No short URL found for the given input' })
     })
   })
 })
